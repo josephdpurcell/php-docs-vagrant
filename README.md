7723e1b26d42d8b1bd2183822d80caa5316175dd
Joseph D. Purcell <josephdpurcell@gmail.com>
2015-07-13 22:48:57 -0500

    Update README

---
+++ README.md
@@ -0,0 +1,18 @@
+# NOTE: This is not functional yet, it's still WIP!
+
+# PHP Docs Vagrant
+
+This Vagrant machine boots with documentation accessible at [http://docs.php.local](http://docs.php.local) and with all dependencies needed for contributing to docs.php.net.
+
+## Requirements
+
+* Vagrant 1.7.2
+
+## Getting Started
+
+1. Add to your /etc/hosts file: `192.168.7.8 docs.php.local`
+1. `git clone git@github.com:josephdpurcell/php-docs-vagrant.git`
+1. `cd php-docs-vagrant`
+1. `vagrant up`
+1. Go to [http://docs.php.local](http://docs.php.local)
+
--- provisioning/bootstrap.sh
+++ provisioning/bootstrap.sh
@@ -16,4 +16,12 @@ cp $PROVISIONING_ROOT/bash/.inputrc /root/.inputrc
 DEBIAN_FRONTEND=noninteractive apt-get -y install curl
 
 # Required for getting php docs.
-DEBIAN_FRONTEND=noninteractive apt-get -y install git svn
+DEBIAN_FRONTEND=noninteractive apt-get -y install git-core subversion git-svn
+
+# Download php docs.
+cd $DOCS_ROOT
+GIT_SSL_NO_VERIFY=true git svn clone https://svn.php.net/repository/phpdoc/modules/doc-en doc-en
+# Note: alternatively, you could store the PEM file: (this is option untested)
+# openssl s_client -showcerts -connect svn.php.net:443 </dev/null 2>/dev/null|openssl x509 -outform PEM > svn.php.net.pem
+# GIT_SSL_CAINFO=svn.php.net.pem git svn clone https://svn.php.net/repository/phpdoc/modules/doc-en doc-en
+
